766461f589df1801351ad815dc0357c6d3e2f968
Nate Graham <avianate@ldschurch.org>
2017-08-10 21:39:43 -0600

    init

---
+++ PropSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PropSchool.MvcApplication" Language="C#" %>
---
+++ PropSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PropSchool.Startup))]
+namespace PropSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
